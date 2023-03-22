7a55615817f71fbe14739ba5a306fd0e81b1baae
Melby S <imel96@yahoo.com>
2023-03-23 01:03:25 +1000

    Scenario: Sell shares
    and replace os/exec with database/sql

--- features/shares.feature
+++ features/shares.feature
@@ -4,10 +4,24 @@ Feature: Shares management
   Scenario: Buy shares
 
     Given there's a trust in the database
-      And the trust has some money in the cash account
+      And the trust has 100 in "cash" account
+      And the trust has 0 in "cost" account
       And there's a broker in the database
       And there's a company in the database
     When the trust buys some shares of the company through the broker
     Then the company shares show up in the trust portfolio
-      And the trust's cash account become empty
-      And the trust's cost account increased
+      And the trust's "cash" account become "0"
+      And the trust's "cost" account become "1"
+
+  Scenario: Sell shares
+
+    Given there's a broker in the database
+      And there's a trust in the database
+      And the trust has 0 in "cash" account
+      And the trust has 0 in "cost" account
+      And the trust has some shares in the trust portfolio
+      And there's a company in the database
+    When the trust sells all shares of the company through the broker
+    Then the trust portfolio shares of the company become "0"
+      And the trust's "cash" account become "100"
+      And the trust's "cost" account become "1"
--- trust_test.go
+++ trust_test.go
@@ -1,16 +1,20 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"crypto/sha256"
+	"database/sql"
 	"fmt"
 	"github.com/cucumber/godog"
-	"os/exec"
+	"log"
+	_ "modernc.org/sqlite"
 	"strings"
 )
 
 const path = "/Users/melbysjamsuddin/Documents/trust_test"
+const debug = false
+
+var Db *sql.DB
 
 type brokerCtxKey struct{}
 type personCtxKey struct{}
@@ -22,84 +26,119 @@ func hashString(text string) string {
 	return fmt.Sprintf("%x", sha256.Sum256([]byte(text)))
 }
 
-func executeQuery(query string) string {
-	return executeQueries([]string{query})
+func queryRow(sql string) string {
+	var out string
+	if debug {
+		log.Printf("%s", sql)
+	}
+	err := Db.QueryRow(sql).Scan(&out)
+	if err != nil {
+		return "error"
+	}
+	return out
 }
 
-func executeQueries(queries []string) string {
-	var sqls string
+func executeQueries(queries ...string) error {
 
 	for _, sql := range queries {
-		sqls = sqls + sql
-		//fmt.Printf("%s\n", sql)
+		if debug {
+			log.Printf("%s", sql)
+		}
+		_, err := Db.Exec(sql)
+		if err != nil {
+			return err
+		}
 	}
-	var out bytes.Buffer
-	cmd := exec.Command("sqlite3", path, sqls)
-	cmd.Stdout = &out
-	cmd.Run()
-	return out.String()
+	return nil
 }
 
 func insertAccount(account_number string, name string, balance int) {
-	executeQueries([]string{
-		"INSERT OR IGNORE INTO account_hub(account_hash, account_number) values('" + hashString(account_number) + "', '" + account_number + "');",
-		"INSERT INTO account_sat(hash, account_hash, name, balance) values('" + hashString(account_number+"|"+name+"|"+fmt.Sprintf("%d", balance)) + "', '" + hashString(account_number) + "', '" + name + "', '" + fmt.Sprintf("%d", balance) + "');",
-	})
+	executeQueries(
+		"INSERT OR IGNORE INTO account_hub(account_hash, account_number) values('"+hashString(account_number)+"', '"+account_number+"');",
+		"INSERT INTO account_sat(hash, account_hash, name, balance) values('"+hashString(strings.Join([]string{account_number, name, fmt.Sprintf("%d", balance)}, "|"))+"', '"+hashString(account_number)+"', '"+name+"', "+fmt.Sprintf("%d", balance)+");",
+	)
 }
 
 func insertCompany(code string, name string, gics string) {
-	executeQueries([]string{
-		"INSERT INTO company_hub(company_hash, code) values('" + hashString(code) + "', '" + code + "');",
-		"INSERT INTO company_sat values('" + hashString(code+"|"+name+"|"+gics) + "', '" + hashString(code) + "', '" + name + "', '" + gics + "');",
-	})
+	executeQueries(
+		"INSERT INTO company_hub(company_hash, code) values('"+hashString(code)+"', '"+code+"');",
+		"INSERT INTO company_sat values('"+hashString(strings.Join([]string{code, name, gics}, "|"))+"', '"+hashString(code)+"', '"+name+"', '"+gics+"');",
+	)
 }
 
 func insertParty(name string, party_type string) {
-	executeQueries([]string{
-		"INSERT INTO party_hub(party_hash, name) values('" + hashString(name) + "', '" + name + "');",
-		"INSERT INTO party_sat(hash, party_hash, party_type) values('" + hashString(name+"|"+party_type) + "', '" + hashString(name) + "', '" + party_type + "');",
-	})
+	executeQueries(
+		"INSERT INTO party_hub(party_hash, name) values('"+hashString(name)+"', '"+name+"');",
+		"INSERT INTO party_sat(hash, party_hash, party_type) values('"+hashString(name+"|"+party_type)+"', '"+hashString(name)+"', '"+party_type+"');",
+	)
 }
 
 func insertLinkWithTwoHubs(link string, a string, b string) {
-	executeQuery("INSERT INTO " + link + " values('" + hashString(a+"|"+b) + "', '" + hashString(a) + "', '" + hashString(b) + "', STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW'));")
+	executeQueries("INSERT INTO " + link + " values('" + hashString(a+"|"+b) + "', '" + hashString(a) + "', '" + hashString(b) + "', STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW'));")
 }
 
 func insertTrustCompanySat(trust string, company string, shareQuantity int) {
-	executeQuery("INSERT INTO trust_company_sat(hash, company_hash, name, gics) values('" + hashString(trust+"|"+company) + "', " + fmt.Sprintf("%d", shareQuantity) + ");")
+	executeQueries("INSERT INTO trust_company_sat(hash, trust_company_hash, share_quantity) values('" + hashString(strings.Join([]string{trust, company, fmt.Sprintf("%d", shareQuantity)}, "|")) + "', '" + hashString(trust+"|"+company) + "', " + fmt.Sprintf("%d", shareQuantity) + ");")
+}
+
+func theTrustPortfolioSharesOfTheCompanyBecome(shareQuantity string) error {
+	out := queryRow("SELECT share_quantity FROM party_hub trust JOIN trust_company_link on (trust_hash = trust.party_hash) JOIN company_hub USING (company_hash) JOIN trust_company_sat USING(trust_company_hash) ORDER BY trust_company_sat.load_time DESC LIMIT 1;")
+
+	if out != shareQuantity {
+		return fmt.Errorf("expected response to be: %s, but actual is: %s", shareQuantity, out)
+	}
+	return nil
+}
+
+func theTrustHasSomeSharesInTheTrustPortfolio() error {
+	insertLinkWithTwoHubs("trust_company_link", "shamsuddins", "AZJ")
+	insertTrustCompanySat("shamsuddins", "AZJ", 10)
+	return nil
+}
+
+func theTrustHasInAccount(amount int, account string) error {
+	switch account {
+	case "cash":
+		insertLinkWithTwoHubs("trust_account_link", "shamsuddins", "1")
+		insertAccount("1", "shamsuddins cash", amount)
+	case "cost":
+		insertLinkWithTwoHubs("trust_account_link", "shamsuddins", "5")
+		insertAccount("5", "shamsuddins cost", amount)
+	}
+	return nil
+}
+
+func theTrustSellsAllSharesOfTheCompanyThroughTheBroker(ctx context.Context) (context.Context, error) {
+	insertLinkWithTwoHubs("trust_company_link", "shamsuddins", "AZJ")
+	insertTrustCompanySat("shamsuddins", "AZJ", 0)
+	insertAccount("1", "shamsuddins cash", 100)
+	insertAccount("5", "shamsuddins cost", 1)
+	ctx = context.WithValue(ctx, trustCostAccountCtxKey{}, "5")
+	return context.WithValue(ctx, trustCashAccountCtxKey{}, "1"), nil
+}
+
+func theTrustsAccountBecome(account, amount string) error {
+	var accountNumber string
+
+	switch account {
+	case "cash":
+		accountNumber = "1"
+	case "cost":
+		accountNumber = "5"
+	}
+	out := queryRow("SELECT account_sat.balance FROM trust_account_link JOIN account_hub USING(account_hash) JOIN account_sat USING(account_hash) WHERE account_hash = '" + hashString(accountNumber) + "' ORDER BY account_sat.load_time DESC LIMIT 1;")
+
+	if strings.TrimRight(out, "\n") != amount {
+		return fmt.Errorf("expected response to be: %s, but actual is: %s", amount, out)
+	}
+	return nil
 }
 
 func theCompanySharesShowUpInTheTrustPortfolio() error {
-	out := executeQuery("select trust.name || \"|\" || company_hub.code from party_hub trust join trust_company_link on (trust_hash = trust.party_hash) JOIN company_hub USING (company_hash);")
+	out := queryRow("SELECT trust.name || \"|\" || company_hub.code FROM party_hub trust JOIN trust_company_link on (trust_hash = trust.party_hash) JOIN company_hub USING (company_hash);")
 
 	if strings.TrimRight(out, "\n") != "shamsuddins|AZJ" {
 		return fmt.Errorf("expected response to be: %s, but actual is: %s", "shamsuddins|AZJ", out)
-	}
-	return nil
-}
-
-func theTrustHasSomeMoneyInTheCashAccount() error {
-	insertAccount("1", "shamsuddins cash", 100)
-	insertAccount("5", "shamsuddins cash", 0)
-	insertLinkWithTwoHubs("trust_account_link", "shamsuddins", "1")
-	insertLinkWithTwoHubs("trust_account_link", "shamsuddins", "5")
-	return nil
-}
-
-func theTrustsCashAccountBecomeEmpty(ctx context.Context) error {
-	out := executeQuery("SELECT account_sat.name || \"|\" || account_sat.balance FROM trust_account_link JOIN account_hub USING(account_hash) JOIN account_sat USING(account_hash) WHERE account_hash = '" + hashString(fmt.Sprintf("%v", ctx.Value(trustCashAccountCtxKey{}))) + "' ORDER BY account_sat.load_time DESC LIMIT 1;")
-
-	if strings.TrimRight(out, "\n") != "shamsuddins cash|0" {
-		return fmt.Errorf("expected response to be: %s, but actual is: %s", "shamsuddins cash|0", out)
-	}
-	return nil
-}
-
-func theTrustsCostAccountIncreased(ctx context.Context) error {
-	out := executeQuery("SELECT account_sat.name || \"|\" || account_sat.balance FROM trust_account_link JOIN account_hub USING(account_hash) JOIN account_sat USING(account_hash) WHERE account_hash = '" + hashString(fmt.Sprintf("%v", ctx.Value(trustCostAccountCtxKey{}))) + "' ORDER BY account_sat.load_time DESC LIMIT 1;")
-
-	if strings.TrimRight(out, "\n") != "shamsuddins cost|1" {
-		return fmt.Errorf("expected response to be: %s, but actual is: %s", "shamsuddins cost|1", out)
 	}
 	return nil
 }
@@ -119,7 +158,7 @@ func theresACompanyInTheDatabase() error {
 }
 
 func thePersonCanBeSeenInTheTrustMemberList(ctx context.Context) error {
-	out := executeQuery("select person.name || \"|\" || trust.name from party_hub person join person_trust_link on (person_hash = person.party_hash) join party_hub trust on (trust_hash = trust.party_hash);")
+	out := queryRow("SELECT person.name || \"|\" || trust.name FROM party_hub person JOIN person_trust_link on (person_hash = person.party_hash) JOIN party_hub trust on (trust_hash = trust.party_hash);")
 
 	if strings.TrimRight(out, "\n") != "melby|shamsuddins" {
 		return fmt.Errorf("expected response to be: %s, but actual is: %s", "melbys|shamsuddins", out)
@@ -147,7 +186,7 @@ func theresATrustInTheDatabase(ctx context.Context) (context.Context, error) {
 }
 
 func theBrokerCanBeSeenInTheTrustBrokerList(ctx context.Context) error {
-	out := executeQuery("select broker.name || \"|\" || trust.name from party_hub broker JOIN broker_trust_link on (broker_hash = broker.party_hash) JOIN party_hub trust on (trust_hash = trust.party_hash);")
+	out := queryRow("SELECT broker.name || \"|\" || trust.name FROM party_hub broker JOIN broker_trust_link on (broker_hash = broker.party_hash) JOIN party_hub trust on (trust_hash = trust.party_hash);")
 
 	if strings.TrimRight(out, "\n") != "cmcmarkets|shamsuddins" {
 		return fmt.Errorf("expected response to be: %s, but actual is: %s", "cmcmarkets|shamsuddins", out)
@@ -171,7 +210,7 @@ func theresABrokerInTheDatabase(ctx context.Context) (context.Context, error) {
 
 func InitializeScenario(ctx *godog.ScenarioContext) {
 	ctx.Before(func(ctx context.Context, sc *godog.Scenario) (context.Context, error) {
-		executeQueries([]string{
+		executeQueries(
 			"DELETE FROM account_hub;",
 			"DELETE FROM account_sat;",
 			"DELETE FROM broker_trust_link;",
@@ -183,27 +222,36 @@ func InitializeScenario(ctx *godog.ScenarioContext) {
 			"DELETE FROM trust_account_link;",
 			"DELETE FROM trust_company_link;",
 			"DELETE FROM trust_company_sat;",
-		})
+		)
 		return ctx, nil
 	})
-	ctx.Step(`^the person can be seen in the trust member list$`, thePersonCanBeSeenInTheTrustMemberList)
-	ctx.Step(`^the person is added to the trust$`, thePersonIsAddedToTheTrust)
-	ctx.Step(`^there\'s a person in the database$`, theresAPersonInTheDatabase)
-	ctx.Step(`^there\'s a trust in the database$`, theresATrustInTheDatabase)
 	ctx.Step(`^the broker can be seen in the trust broker list$`, theBrokerCanBeSeenInTheTrustBrokerList)
 	ctx.Step(`^the broker is added to the trust$`, theBrokerIsAddedToTheTrust)
-	ctx.Step(`^there\'s a broker in the database$`, theresABrokerInTheDatabase)
-	ctx.Step(`^the trust buys some shares of the company through the broker$`, theTrustBuysSomeSharesOfTheCompanyThroughTheBroker)
-	ctx.Step(`^there\'s a company in the database$`, theresACompanyInTheDatabase)
 	ctx.Step(`^the company shares show up in the trust portfolio$`, theCompanySharesShowUpInTheTrustPortfolio)
-	ctx.Step(`^the trust has some money in the cash account$`, theTrustHasSomeMoneyInTheCashAccount)
-	ctx.Step(`^the trust\'s cash account become empty$`, theTrustsCashAccountBecomeEmpty)
-	ctx.Step(`^the trust\'s cost account increased$`, theTrustsCostAccountIncreased)
+	ctx.Step(`^the person can be seen in the trust member list$`, thePersonCanBeSeenInTheTrustMemberList)
+	ctx.Step(`^the person is added to the trust$`, thePersonIsAddedToTheTrust)
+	ctx.Step(`^the trust buys some shares of the company through the broker$`, theTrustBuysSomeSharesOfTheCompanyThroughTheBroker)
+	ctx.Step(`^the trust has (\d+) in "([^"]*)" account$`, theTrustHasInAccount)
+	ctx.Step(`^the trust has some shares in the trust portfolio$`, theTrustHasSomeSharesInTheTrustPortfolio)
+	ctx.Step(`^the trust portfolio shares of the company become "([^"]*)"$`, theTrustPortfolioSharesOfTheCompanyBecome)
+	ctx.Step(`^the trust sells all shares of the company through the broker$`, theTrustSellsAllSharesOfTheCompanyThroughTheBroker)
+	ctx.Step(`^the trust\'s "([^"]*)" account become "([^"]*)"$`, theTrustsAccountBecome)
+	ctx.Step(`^there\'s a broker in the database$`, theresABrokerInTheDatabase)
+
+	ctx.Step(`^there\'s a company in the database$`, theresACompanyInTheDatabase)
+	ctx.Step(`^there\'s a person in the database$`, theresAPersonInTheDatabase)
+	ctx.Step(`^there\'s a trust in the database$`, theresATrustInTheDatabase)
 }
 
 func FeatureContext(s *godog.TestSuiteContext) {
 	s.BeforeSuite(func() {
-		executeQueries([]string{
+		var err error
+		Db, err = sql.Open("sqlite", path)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		executeQueries(
 			"CREATE TABLE IF NOT EXISTS account_hub(account_hash text unique, account_number text, load_time timestamp DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')));",
 			"CREATE TABLE IF NOT EXISTS account_sat(hash text unique, account_hash text, name text, balance int, load_time timestamp DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')));",
 			"CREATE TABLE IF NOT EXISTS broker_trust_link(broker_trust_hash text unique, broker_hash text, trust_hash text, load_time timestamp);",
@@ -214,7 +262,7 @@ func FeatureContext(s *godog.TestSuiteContext) {
 			"CREATE TABLE IF NOT EXISTS person_trust_link(person_trust_hash text unique, person_hash text, trust_hash text, load_time timestamp);",
 			"CREATE TABLE IF NOT EXISTS trust_account_link(trust_account_hash text unique, trust_hash text, account_hash text, load_time timestamp);",
 			"CREATE TABLE IF NOT EXISTS trust_company_link(trust_company_hash text unique, trust_hash text, company_hash text, load_time timestamp);",
-			"CREATE TABLE IF NOT EXISTS trust_company_sat(trust_company_hash text unique, shareQuantity int, load_time timestamp DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')));",
-		})
+			"CREATE TABLE IF NOT EXISTS trust_company_sat(hash text unique, trust_company_hash text, share_quantity int, load_time timestamp DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')));",
+		)
 	})
 }
